3e8963a111f25529bce81d405d8ea996f50b323d
Gabriel Willian <gabriel.williancontato1999@gmail.com>
2025-08-28 07:03:55 -0300

    add comments

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
 int main(){
-
+    //Declaro as variaveis usadas no programa
     int numero;
     int soma = 0;
-
+    //Boas vindas ao usuario
     printf("***Calculadora de números inteiros***\n\n");
     printf("Digite apenas número inteiros\n");
     printf("Caso queira encerrar o programa digite 0\n\n");
-
+    //começo a estrutura de repetição While
     while(1){
+        //faço a solicitação ao usuario
         printf("Digite um número inteiro: ");
         scanf("%d", &numero);
+        //verifico se o número digitado é o número sentinela "0" que interrompe o programa.
         if(numero == 0)
         {
             break;
         }
+        //verifico se o usuario digitou algum número negativo
         if(numero < 0){
             printf("Digite um número positivo...\n");
             continue;
         }
+        //faço a soma
         soma += numero;
     }
+    //mostro ao usuario o resultado da soma dos números
     printf("\nPrograma encerrado com sucesso...\nA soma dos números foi de: %d\n", soma);
     return 0;
 }
